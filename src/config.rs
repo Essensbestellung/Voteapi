75e44f88665efe3f20a3c01cad603266c3c1f5c8
fabian <pfafff@dhbw-loerrach.de>
2022-04-01 19:01:46 +0200

    Added Module to read params form env

---
+++ src/config.rs
@@ -0,0 +1,16 @@
+const DEFAULT_REDIS_HOST: &str = "192.168.1.188:6379";
+const DEFAULT_REDIS_PORT: &str = "6369";
+
+
+pub fn get_redis_host() -> String {
+    option_env!("REDIS_HOST").unwrap_or(DEFAULT_REDIS_HOST).to_string()
+}
+
+pub fn get_redis_port() -> String {
+    option_env!("REDIS_PORT").unwrap_or(DEFAULT_REDIS_PORT).to_string()
+}
+
+
+pub fn get_connection_string() -> String{
+    format!("{}:{}", get_redis_host(), get_redis_port())
+}
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ mod routes;
 mod endpoints;
 mod db;
 mod model;
-// this function could be located in a different module
-
-const REDIS_CON_STRING: &str = "192.168.1.188:6379";
-
+mod config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,7 +18,8 @@ async fn main() -> std::io::Result<()> {
     /* I have no idea why the move is needed */
     /* according to documentation it is needed to move the AppData into the Closure*/
     HttpServer::new( move || {
-        let redis_addr = RedisActor::start(REDIS_CON_STRING);
+        let redis_addr = RedisActor::start(config::get_connection_string());
+        log::info!("Connection String: {}" , config::get_connection_string());
         App::new()
             .app_data(web::Data::new(redis_addr))
             .service(web::scope("/vote").configure(routes::route_cast).configure(routes::route_result))
